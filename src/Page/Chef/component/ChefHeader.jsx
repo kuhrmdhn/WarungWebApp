ada39f4421b6054eabaa3f6b3da3c8ee048d2333
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-03-05 09:51:15 +0700

    Feat/chef (#8)
    * chef: editing ui, order group card, and optimize search order group

--- src/Page/Chef/component/ChefHeader.jsx
+++ src/Page/Chef/component/ChefHeader.jsx
@@ -6,10 +6,10 @@ function ChefHeader() {
     const setSearchOrderGroupItem = useSearchKeyword(state => state.setSearchOrderGroupItem)
 
     return (
-        <header className="w-full h-16 sm:h-24 lg:h-1/6 bg-chef-primary flex justify-around items-center">
-            <Logo.LogoWhite />
+        <header className="w-full h-16 sm:h-24 lg:h-1/6 bg-white flex justify-around items-center">
+            <Logo.LogoBlack />
             <span className="w-1/2 sm:w-1/3 lg:w-1/5 h-10">
-            <SearchBar setKeyword={setSearchOrderGroupItem} placeholder={"Cari Nomor Pesanan"} />
+            <SearchBar setKeyword={setSearchOrderGroupItem} placeholder={"Cari Nama Pembeli"} />
             </span>
         </header>
     )
--- src/Page/Chef/component/ChefOrderList.jsx
+++ src/Page/Chef/component/ChefOrderList.jsx
@@ -5,15 +5,14 @@ import OrderGroupCard from "../element/OrderGroupCard";
 function ChefOrderList() {
     const searchOrderGroupItem = useSearchKeyword(state => state.searchOrderGroupItem)
     const orderGroupData = useGetApiStore(state => state.orderGroupData)
-    let filterOrderGroup;
-    if (searchOrderGroupItem != "") {
-        filterOrderGroup = orderGroupData.filter(dataGroup => dataGroup.id == searchOrderGroupItem)
-    } else {
-        filterOrderGroup = orderGroupData
+    const convertSearchInput = searchOrderGroupItem.toLowerCase().replaceAll(/ /g, "")
+    let filterOrderGroup = orderGroupData
+    if (searchOrderGroupItem !== "") {
+        filterOrderGroup = orderGroupData.filter(dataGroup => dataGroup.buyer.toLowerCase().replaceAll(/ /g, "").includes(convertSearchInput))
     }
 
     return (
-        <section className="w-full h-5/6 bg-black pt-5 sm:pt-7 flex justify-center items-start webkit-scroll-bar">
+        <section className="w-full h-5/6 pt-5 sm:pt-7 flex justify-center items-start overflow-y-small">
             <div className="min-h-fit w-11/12 grid grid-cols-2 sm:grid-cols-3 gap-3">
                 {
                     filterOrderGroup.map((order) => (
