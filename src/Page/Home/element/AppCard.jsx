6b2e3ec95076fc64b196d56ece436d066767880a
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-04-02 11:59:18 +0700

    Feat/home (#13)
    * manage utility on state management and component
    
    * edit: edit app card ui, and add lazy loading for app list

--- src/Page/Home/element/AppCard.jsx
+++ src/Page/Home/element/AppCard.jsx
@@ -1,14 +1,15 @@
 import { Launch } from "@mui/icons-material"
 import { Button } from "@mui/material"
+import PropTypes from "prop-types"
 
 function AppCard({ link, img, alt }) {
     function movePage() {
         window.open(link, "_blank")
     }
     return (
-        <section className="w-56 sm:w-60 relative font-raleway">
-            <img className="aspect-auto h-5/6" src={img} alt={alt} />
-            <div className="h-1/6 w-full mt-3 flex justify-around items-center text-lg">
+        <section className="w-56 sm:w-60 relative font-raleway hover:scale-105 duration-500 cursor-pointer">
+            <img className="h-1/2 w-full" src={img} alt={alt} />
+            <div className="h-1/6 w-full mt-3 absolute bottom-0 backdrop-blur-sm text-white flex justify-around items-center text-lg">
                 <h1>{alt}</h1>
                 <Button onClick={movePage} variant="contained" color="primary"><Launch sx={{ fontSize: "18px" }} /></Button>
             </div>
@@ -17,3 +18,9 @@ function AppCard({ link, img, alt }) {
 }
 
 export default AppCard
+
+AppCard.propTypes = {
+    link: PropTypes.string,
+    img: PropTypes.string,
+    alt: PropTypes.string
+}
--- src/Page/Home/pages/HomePage.jsx
+++ src/Page/Home/pages/HomePage.jsx
@@ -1,11 +1,15 @@
-import AppList from "../component/AppList"
+import { Suspense, lazy } from "react"
+const AppList = lazy(() => import("../component/AppList"))
 import HomeHeader from "../component/HomeHeader"
+import AppCardSkeleton from "../../../skeleton/AppCardSkeleton"
 
 function HomePage() {
   return (
     <main className="h-screen w-full bg-white text-black">
-      <HomeHeader/>
-      <AppList/>
+      <HomeHeader />
+      <Suspense fallback={<AppCardSkeleton />}>
+        <AppList />
+      </Suspense>
     </main>
   )
 }
