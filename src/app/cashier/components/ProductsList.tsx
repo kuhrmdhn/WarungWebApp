527fb3fa72b7ad2f6e26011c9a144bc6584906b8
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-05-03 21:36:36 +0700

    Page/cashier (#27)
    * fix: pagination with query params when searching products

--- src/app/cashier/components/ProductsList.tsx
+++ src/app/cashier/components/ProductsList.tsx
@@ -2,14 +2,21 @@
 import ProductCard from '@/app/ui/ProductCard'
 import { Product } from '@/lib/interface/productInterface'
 import { ProductsStore } from '@/lib/store/productsStore'
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
 export default function ProductsList() {
-    const { filteredProducts } = ProductsStore()
+    const { products, filteredProducts } = ProductsStore()
+    const searchParams = useSearchParams()
+    let productsData = filteredProducts
+    if(searchParams.get("query")) {
+        const res = products.filter((products: Product) => products.name.toLowerCase().includes(searchParams.get("query")?.toString() || ''))
+        productsData = res
+    }
     return (
         <section className='min-h-[75svh] w-full mt-2 sm:mt-7 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 justify-items-center gap-y-2 sm:gap-y-7'>
             {
-                filteredProducts.map((product: Product, index: number) => (
+                productsData.map((product: Product, index: number) => (
                     <ProductCard
                         key={index}
                         productData={product}
