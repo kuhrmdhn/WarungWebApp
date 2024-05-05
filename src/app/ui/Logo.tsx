423869bd558d1fe27ee37a5c4c7ae1807085b042
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-05-05 18:53:07 +0700

    Page/cashier (#28)
    * edit: logo color => black and white

--- src/app/ui/Logo.tsx
+++ src/app/ui/Logo.tsx
@@ -2,10 +2,22 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function Logo() {
+function Logo({children}: {children: React.ReactElement<HTMLElement>}) {
     return (
         <a href="/">
-            <Image priority height={70} width={125} className='h-8 sm:h-auto w-auto' alt='Warung Web logo Icon' src={"/Logo.webp"} />
+            {children}
         </a>
     )
 }
+
+function LogoWhite() {
+    return <Image priority height={70} width={125} className='h-8 sm:h-auto w-auto' alt='Warung Web logo Icon' src={"/Logo-White.webp"} />
+}
+
+function LogoBlack() {
+    return <Image priority height={70} width={125} className='h-8 sm:h-auto w-auto' alt='Warung Web logo Icon' src={"/Logo-Black.webp"} />
+}
+
+Logo.LogoBlack = LogoBlack
+Logo.LogoWhite = LogoWhite
+export default Logo
