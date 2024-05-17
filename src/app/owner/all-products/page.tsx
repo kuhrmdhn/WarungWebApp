cd0aae21a021443bfb06034550cb0049e15bc9a5
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-05-17 21:24:37 +0700

    add Suspense in /all-products to handle useSearchParams

--- src/app/owner/all-products/page.tsx
+++ src/app/owner/all-products/page.tsx
@@ -1,11 +1,11 @@
 import ProductsList from '@/app/cashier/components/ProductsList'
 import SearchBar from '@/app/ui/SearchBar'
-import React from 'react'
+import React, { Suspense } from 'react'
 import NavigationBar from '../components/NavigationBar'
 
 export default function AllProducts() {
   return (
-    <>
+    <Suspense fallback={<h1>Loading...</h1>}>
       <NavigationBar />
       <main id='allProduct' className='min-h-screen h-max w-full flex flex-col'>
         <section className="w-11/12 sm:w-1/3 h-max self-end mr-5">
@@ -15,6 +15,6 @@ export default function AllProducts() {
           <ProductsList />
         </section>
       </main>
-    </>
+    </Suspense>
   )
 }
