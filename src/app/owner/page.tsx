ac0ca16dccfd77e886305717429fdfb82b7b31e8
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-05-31 20:55:38 +0700

    bug fix in Page Title component

--- src/app/owner/page.tsx
+++ src/app/owner/page.tsx
@@ -10,7 +10,9 @@ export default function OwnerDashboard() {
       <NavigationBar />
       <main className='bg-body-gray min-h-[87svh] h-max w-full px-2 sm:px-5 pt-3 sm:pt-7'>
         <PageTitle>
-          <PageTitle.Title text='Dashboard' />
+          <PageTitle.Title>
+            Dashboard
+          </PageTitle.Title>
         </PageTitle>
         <Summary />
         <SalesReport />
