820843a37cc6d66086b877333dc983318e040e1b
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-06-28 23:22:47 +0700

    add login page with responsive and full functions

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,16 +2,44 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { getSession, signIn } from 'next-auth/react'
-import Image from 'next/image'
-import { Button, FormLabel, Input, InputGroup } from '@chakra-ui/react'
+import { Button, FormLabel, Input, InputGroup, useToast } from '@chakra-ui/react'
 import { Session } from '@/lib/interface/token'
+import { ArrowForward, Error, HourglassBottom } from '@mui/icons-material'
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({ username: "", password: "" })
+  const [pending, setPending] = useState(false)
   const { push } = useRouter()
+  const toast = useToast()
+
+  const inputData = [
+    {
+      label: "Username",
+      type: "text",
+      placeholder: "John Doe",
+      value: formData.username
+    },
+    {
+      label: "Password",
+      type: "password",
+      placeholder: "********",
+      value: formData.password
+    },
+  ]
 
   async function handleLogin(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     e.preventDefault()
+    if (formData.username === "" || formData.password === "") {
+      toast({
+        title: "Input can't empty",
+        colorScheme: "red",
+        icon: <Error />,
+        duration: 3000,
+        position: "top-right"
+      })
+      return
+    }
+    setPending(true)
     const res = await signIn('credentials', {
       redirect: false,
       username: formData.username,
@@ -36,25 +64,56 @@ export default function LoginPage() {
 
     setFormData((current) => ({ ...current, [name]: value }))
   }
+  // 
 
   return (
-    <section className="h-[100svh] w-full flex justify-around items-center">
-      <Image src={"/auth-icon.svg"} alt="Authentication Image" height={10} width={20} className='w-1/5 aspect-square' />
-      <form className="w-1/3 h-full flex justify-center items-center flex-col gap-3">
-        <InputGroup className="flex justify-center items-center gap-5">
-          <FormLabel className='w-1/2'>
-            Username
-          </FormLabel>
-          <Input type="text" placeholder='John Doe' name='username' value={formData.username} onChange={(e) => handleOnChange(e)} />
-        </InputGroup>
-        <InputGroup className="flex justify-center items-center gap-5">
-          <FormLabel className='w-1/2'>
-            Password
-          </FormLabel>
-          <Input type='password' placeholder='johndoe' name="password" value={formData.password} onChange={(e) => handleOnChange(e)} />
-        </InputGroup>
-        <Button onClick={(e) => handleLogin(e)} type="submit" colorScheme='blue' className="w-1/4 self-end">Submit</Button>
+    <section className="h-[100svh] w-full flex justify-center lg:justify-end items-center bg-[url('/login-bg.webp')] bg-cover bg-fixed bg-no-repeat bg-right-bottom">
+      <form className="w-full sm:w-2/3 lg:w-1/3 h-full rounded-md flex justify-center items-start flex-col gap-3 px-9 lg:px-4 lg:mr-7 text-white">
+        <div className="h-1/5">
+          <h1 className="text-4xl">Welcome Back!</h1>
+          <p className='text-gray-100'>Make sure you login as your position</p>
+        </div>
+        {
+          inputData.map((input, index: number) => (
+            <InputGroup
+              key={index}
+              className="flex justify-center items-center gap-5 mb-3 lg:mb-7 h-10 lg:h-14"
+            >
+              <FormLabel className='w-1/2 lg:w-1/4 cursor-pointer text-sm sm:text-base' htmlFor={input.label}>
+                {input.label}
+              </FormLabel>
+              <Input
+                id={input.label}
+                placeholder={input.placeholder}
+                name={input.label.toLowerCase()}
+                type={input.type}
+                value={input.value}
+                backgroundColor="transparent"
+                focusBorderColor='transparent'
+                className='border-0 border-b-2 border-double rounded-none focus:border-b-white text-sm sm:text-base'
+                onChange={(e) => handleOnChange(e)} />
+            </InputGroup>
+          ))
+        }
+        <Button
+          onClick={(e) => handleLogin(e)}
+          type="submit"
+          borderRadius={"30px"}
+          size={"lg"}
+          disabled={pending}
+          className={`w-full flex justify-between items-center px-9 ${pending && "cursor-wait"} mt-5 lg:mt-0 text-sm sm:text-base`}
+        >
+          {pending ? "Please Wait" : "Continue"}
+          {
+            pending ?
+              <HourglassBottom className="text-sm sm:text-base" /> :
+              <ArrowForward className="text-sm sm:text-base" />
+          }
+        </Button>
       </form>
+      <div className="fixed bottom-4 left-2 z-10 text-gray-200 text-2xs">
+        Photo by <a href="https://unsplash.com/@ninjason?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jason Leung</a> on <a href="https://unsplash.com/photos/photo-of-pub-set-in-room-during-daytime-poI7DelFiVA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      </div>
     </section>
   )
 }
