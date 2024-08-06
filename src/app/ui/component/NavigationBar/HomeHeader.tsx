e765d315bead876840f7207bf7b131611f7572bd
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-08-06 11:45:31 +0700

    editing home layout

--- src/app/ui/component/NavigationBar/HomeHeader.tsx
+++ src/app/ui/component/NavigationBar/HomeHeader.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import LogoBlack from '../Logo/LogoBlack'
 import { Button } from '@chakra-ui/react'
+import { signOut } from 'next-auth/react'
+import { useSession } from '@/hooks/useSession'
 
-export default function Header() {
+export default async function Header() {
+    const session = await useSession()
     return (
         <header className="w-full h-16 px-16 bg-white flex justify-between items-center fixed top-0 left-0 z-50 border-b-2">
             <LogoBlack />
-            <a href="/login">
-                <Button colorScheme='teal'>Login</Button>
-            </a>
+            {
+                session ? <Button onClick={() => signOut()}>Logout</Button> :
+                    <a href="/login">
+                        <Button colorScheme='teal'>Login</Button>
+                    </a>
+            }
         </header>
     )
 }
