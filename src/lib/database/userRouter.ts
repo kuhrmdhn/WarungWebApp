981f0071b71bd9d4815c2b3c5b893cbd27129d8b
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-08-30 21:12:30 +0700

    Page/owner (#103)
    * add owner user management page

--- src/lib/database/userRouter.ts
+++ src/lib/database/userRouter.ts
@@ -1,8 +1,35 @@
 import { bcryptConfig } from "@/config/bcrypt";
 import { supabase } from "@/config/supabase";
 import { User } from "@/types/userInterface";
+import { userManageStore } from "../store/userManageStore";
 
 export const userRouter = {
+    async getAllUser() {
+        try {
+            const { data: users, error } = await supabase.from("users").select()
+            if (error) {
+                throw new Error(error.message)
+            }
+            userManageStore.getState().setAllUser(users)
+            return users
+        } catch (error) {
+            console.error(error);
+            return error
+        }
+    },
+    async deleteUser(userId: string) {
+        try {
+            const { error, status } = await supabase.from("users").delete().eq("id", userId)
+            if(error) {
+                throw new Error(error.message)
+            }
+            userRouter.getAllUser()
+            return status
+        } catch (error) {
+            console.error(error)
+            return error
+        }
+    },
     async getUser(username: string, password: string) {
         if (!username || !password) {
             throw new Error("Password and Username is Required!")
