4dd267b472384ad5e2d808df012a6ebe05c3dd9e
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2025-01-07 14:47:40 +0700

    [fix] owner form page layout (#120)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,5 @@ import HomePage from "./_home/page";
 import "./globals.css";
 
 export default function Home() {
-  return (
-    <HomePage/>
-  );
+  return <HomePage />;
 }
--- src/ui/component/ProductList/ProductsList.tsx
+++ src/ui/component/ProductList/ProductsList.tsx
@@ -36,7 +36,7 @@ export default function ProductsList({ className, renderCard }: productListProps
     return (
         <section className={`min-h-[75svh] w-full mt-2 sm:mt-7 grid ${className}`}>
             {
-                products.map((product: Product, index: number) => (
+                products.sort((a,b) => a.id - b.id).map((product: Product, index: number) => (
                     <FadeInUp key={index} delay={index * 0.1}>
                         {renderCard(product)}
                     </FadeInUp>
